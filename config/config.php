9eb612e0c20b7f5a9b3c5e7fdd4c800b8d3c0bb6
Trần Văn Dương <56970251+duongspm@users.noreply.github.com>
2021-07-12 21:58:23 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,8 @@
+
 <?php
-    $hostName = 'MYSQL5047.site4now.net';
-// khai báo biến username
-$userName = 'a77512_tranvan';
-//khai báo biến password
-$passWord = 'shop_nhom3';
-// khai báo biến databaseName
-$databaseName = 'db_a77512_tranvan';
-$conn = mysqli_connect($hostName, $userName, $passWord , $databaseName) or die ('Không thể kết nối tới database');
+    define("DB_HOST","MYSQL5047.site4now.net");
+    define("DB_USER","a77512_tranvan");
+    define("DB_PASS","shop_nhom3");
+    define("DB_NAME","db_a77512_tranvan");
 ?>
+
